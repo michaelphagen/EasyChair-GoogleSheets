0a790da3c89b085ee6dde245534f5c2c3c9be445
Michael Hagen <michaelhagen@nyu.edu>
2023-08-01 15:40:25 -0400

    Include Calendar pulling function

--- Code.js
+++ Code.js
@@ -26,19 +26,135 @@ function onOpen() {
         page.setName("var");
         page.getRange(1, 1).setValue("Submission Sheet");
         page.getRange(2, 1).setValue("Conference Data Export Sheet");
+        page.getRange(3, 1).setValue("Schedule Sheet");
     }
     var ui = SpreadsheetApp.getUi();
+    // Create update sheets menu
     ui.createMenu('Update Sheets')
         .addItem('Select Spreadsheets', 'selectSheets')
         .addItem('Update sheets', 'updateSheetsOverwrite')
         //.addItem('Update sheets and do not overwrite', 'updateSheetsNoOverwrite')
         .addToUi();
+    // Create pull schedule menu
+    ui.createMenu('Pull Schedule')
+        .addItem('Select Schedule', 'selectSchedule')
+        .addItem('Pull Schedule', 'writeSchedule')
+        .addToUi();
+}
+
+function selectSchedule() {
+    var page = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("var");
+     // Create the UI for selecting the schedule sheet id
+    var ui = SpreadsheetApp.getUi();
+    var response = ui.prompt('Enter the ID of the schedule sheet', ui.ButtonSet.OK_CANCEL);
+    // Process the user's response.
+    if (response.getSelectedButton() == ui.Button.OK) {
+        var scheduleURL = response.getResponseText();
+        page.getRange(3, 2).setValue(scheduleURL);
+    }
+}
+
+// Get the Scheduled room and time from the shedule sheet
+function getSchedule(sessionTitle) {
+    // Get the schedule sheet URL
+    var page = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("var");
+    var scheduleSheetID = page.getRange(3, 2).getValue();
+    // Get the schedule sheet
+    var scheduleSheet = SpreadsheetApp.openById(scheduleSheetID).getActiveSheet();
+    // Get the schedule sheet data
+    var scheduleSheetData = scheduleSheet.getDataRange().getValues();
+    // Find the sessionTitle in the schedule sheet
+    for (var y = 0; y < scheduleSheetData.length; y++) {
+        for (var x = 0; x < scheduleSheetData[0].length; x++) {
+            if (scheduleSheetData[y][x] == sessionTitle) {
+                // Get the scheduled room and time
+                var scheduledRoom = scheduleSheetData[y][1];
+                var scheduledTime = scheduleSheetData[1][x];
+                // if scheduled time is empty, get the time one column to the left
+                if (scheduledTime == "") {
+                    scheduledTime = scheduleSheetData[1][x - 1];
+                    var add15 = true;
+                }
+                var time = new Date(scheduledTime);
+                time.setHours(time.getHours() - 3);
+                if (add15) {
+                    time.setMinutes(time.getMinutes() + 15);
+                }
+                scheduledTime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+                // Date is in the first column, search upwards for the word "October" to find the date
+                var day = String(scheduleSheetData[y][0]);
+                for (var i = y; i > 0; i--) {
+                    // This could be converted to a date comparison
+                    if (String(scheduleSheetData[i][0]).includes("Oct")) {
+                        day = String(scheduleSheetData[i][0]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // If a day was found, get just the Month and Day
+    if (day != null) {
+        day = day.split(" ")[1] + " " + day.split(" ")[2];
+    }
+    // Return the scheduled room and time
+    return [scheduledRoom, scheduledTime, day];
+}
+
+function writeSchedule() {
+    // Go through this spreadsheet and get the scheduledRoom, scheduledTime for each session, then print it
+    // Get the spreadsheet
+    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    sheet = spreadsheet.getSheetByName("View");
+    // Get the data
+    var data = spreadsheet.getDataRange().getValues();
+    for (var y = 0; y < data.length; y++) {
+        var title = "#" + data[y][0] + " " + data[y][1];
+        var [scheduledRoom, scheduledTime, date] = getSchedule(title);
+        // Write the scheduledRoom, date+scheduledTime to the spreadsheet in new last columns
+        // If any are undefined, don't write anything
+        if (scheduledRoom == undefined || scheduledTime == undefined || date == undefined) {
+            continue;
+        }
+        else {
+            var row = y + 1;
+            // Check if roomColumn exists
+            var roomColumn = headerSearch(data[0], "Room");
+            if (roomColumn == undefined) {
+                roomColumn = data[0].length + 1;
+                data[0].push("Room");
+                sheet.getRange(1, roomColumn).setValue("Room");
+            }
+            // Check if timeColumn exists
+            var timeColumn = headerSearch(data[0], "Time");
+            if (timeColumn == undefined) {
+                timeColumn = data[0].length + 1;
+                data[0].push("Time");
+                sheet.getRange(1, timeColumn).setValue("Time");
+            }
+            // Check if dateColumn exists
+            var dateColumn = headerSearch(data[0], "Date");
+            if (dateColumn == undefined) {
+                dateColumn = data[0].length + 1;
+                data[0].push("Date");
+                sheet.getRange(1, dateColumn).setValue("Date");
+            }
+            sheet.getRange(row, roomColumn).setValue(String(scheduledRoom));
+            sheet.getRange(row, timeColumn).setValue(scheduledTime);
+            sheet.getRange(row, dateColumn).setValue(date);
+        }
+    }
+}
+// I'm not sure why array.find doesn't work, but this does
+function headerSearch(arr, val) {
+    for (var i = 0; i < arr.length; i++)
+        if (arr[i] == val) return i+1;
+    return undefined;
 }
 
 // Path: Code.js
 // Select the spreadsheets to source data from
 function selectSheets() {
-    // Get the spreadsheets from var page if they exist
     var page = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("var");
     if (page != null) {
         // Column 2, row 1 is the id of the submission sheet
@@ -60,7 +176,7 @@ function selectSheets() {
             page.getRange(1, 2).setValue(id);
         }
     }
-// Create the UI for selecting the conferenceDataExportSheetId
+    // Create the UI for selecting the conferenceDataExportSheetId
     var ui = SpreadsheetApp.getUi();
     var response = ui.prompt(
         'Enter the ID of the conferenceDataExportSheet to source data from',
@@ -87,7 +203,7 @@ function updateSheetsOverwrite() {
     buildViewPage();
 }
 
-function importSubmissionSheet(){
+function importSubmissionSheet() {
     //get submission sheet id
     var page = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("var");
     if (page != null) {
@@ -107,7 +223,7 @@ function importSubmissionSheet(){
     submissionsSheet.getRange(1, 1, submissionSheetData.length, submissionSheetData[0].length).setValues(submissionSheetData);
 }
 
-function importConferenceDataExportSheet(){
+function importConferenceDataExportSheet() {
     // get conference data export sheet id
     var page = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("var");
     if (page != null) {
@@ -147,6 +263,7 @@ function getColumnByName(viewSheetData, title) {
     return titleColumn;
 }
 
+
 // Copy columns
 function copyColumns(sourceSheet, destinationSheet, columnName) {
     // Find the column number of the column with the title "Title" in the view sheet
@@ -177,7 +294,7 @@ function buildViewPage() {
     var authorsData = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("conferenceDataAuthors").getDataRange().getValues();
     // Write column "#" of main to column "Submission ID" of view
     if (viewSheetData[0][0] != "#") {
-        viewSheetData[0] = ["#", "Title", "Authors", "Emails","Related Tracks","Duration","A/V","Other equipment","Special Requests","abstract"];
+        viewSheetData[0] = ["#", "Title", "Authors", "Emails", "Related Tracks", "Duration", "A/V", "Other equipment", "Special Requests","abstract"];
     }
     // Copy the submission id column from the submissions sheet to the view data
     for (var i = 1; i < submissionsData.length; i++) {
@@ -216,7 +333,7 @@ function buildViewPage() {
                 }
 
             }
-        viewSheetData[i][viewAuthorsColumn] = authors;
+            viewSheetData[i][viewAuthorsColumn] = authors;
         }
     }
     // Copy the emails column from the submissions sheet to the view data. Append if there is already an email
@@ -235,9 +352,7 @@ function buildViewPage() {
         viewSheetData[i][viewEmailsColumn] = emails;
     }
     viewSheetData = copyColumns(conferenceDataExportData, viewSheetData, "abstract");
-
-
-
+    // Get the calendar data
     // write view data to view sheet
     SpreadsheetApp.getActiveSpreadsheet().getSheetByName("view").getRange(1, 1, viewSheetData.length, viewSheetData[0].length).setValues(viewSheetData);
 
